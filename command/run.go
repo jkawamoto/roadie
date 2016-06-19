943bf435868891450b8b04d7a949cddfc0874cc0
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-19 19:20:29 -0400

    Show warnings when any source flag is given even yaml file has source section.

--- command/run.go
+++ command/run.go
@@ -3,9 +3,12 @@ package command
 import (
 	"bytes"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 	"text/template"
 
+	"github.com/ttacon/chalk"
 	"github.com/urfave/cli"
 	"gopkg.in/yaml.v2"
 )
@@ -30,29 +33,56 @@ func CmdRun(c *cli.Context) error {
 	conf := make(config)
 	err := loadScript(c.Args()[0], c.StringSlice("e"), &conf)
 	if err != nil {
-		return err
+		return cli.NewExitError(err.Error(), 2)
 	}
 
 	// Prepare source section.
 	if v := c.String(git); v != "" {
+
 		if _, ok := conf[source]; ok {
-			// fmt.Println("Although %s has source element, another repository is given.", c.Args()[0])
+			log.Printf(
+				chalk.Red.Color("%s has source section but a Git repository is given. The source section will be overwritten to '%s'."),
+				c.Args()[0],
+				v,
+			)
 		}
 		conf[source] = v
+
 	} else if v := c.String(url); v != "" {
+
 		if _, ok := conf[source]; ok {
+			log.Printf(
+				chalk.Red.Color("%s has source section but a repository URL is given. The source section will be overwritten to '%s'."),
+				c.Args()[0],
+				v,
+			)
 
 		}
 		conf[source] = v
-	} else if v := c.String(local); v != "" {
+
+	} else if path := c.String(local); path != "" {
+
 		if _, ok := conf[source]; ok {
+			log.Printf(
+				chalk.Red.Color("%s has source section but a path for source codes is given. The source section will be overwritten."),
+				c.Args()[0],
+			)
+		}
+
+		if info, notExists := os.Stat(path); notExists != nil {
+			// TODO: File doesn't exist.
+		} else if info.IsDir() {
+			// TODO: Make a tar ball and upload it to a bucket.
+		} else {
+			// TODO: If v is a already archived file, just upload it.
+			if !strings.HasSuffix(path, ".zip") && !strings.HasSuffix(path, ".tar") && !strings.HasSuffix(path, ".tar.gz") {
+				// TODO: Warning.
+			}
 
 		}
-		conf[source] = v
 
-		// TODO: If v is a already archived file, just upload it.
-
-		// TODO: Make a tar ball and upload it to a bucket.
+		// TODO: path must be updated to a valid gs url.
+		conf[source] = path
 
 	} else {
 		// TODO: if no source flag given, what shoud it do?
@@ -89,17 +119,21 @@ func loadScript(filename string, args []string, out *config) error {
 	// Load YAML config file.
 	conf, err := template.New(filename).Funcs(funcs).ParseFiles(filename)
 	if err != nil {
-		return cli.NewExitError(err.Error(), 2)
+		switch err.(type) {
+		case *os.PathError:
+			return err
+		default:
+			return fmt.Errorf("Cannot apply variables to the place holders in %s", filename)
+		}
 	}
 
 	// Replace place holders with given args.
 	buf := &bytes.Buffer{}
 	if err := conf.Execute(buf, nil); err != nil {
-		return cli.NewExitError(err.Error(), 2)
+		return err
 	}
 
 	// Unmarshal YAML file.
-	fmt.Println(buf.String())
 	return yaml.Unmarshal(buf.Bytes(), out)
 
 }
@@ -107,17 +141,31 @@ func loadScript(filename string, args []string, out *config) error {
 // checkResultSection validates config has result section.
 func checkResultSection(c *cli.Context, conf *config) error {
 
-	if _, ok := conf[result]; !ok {
-
-		// if c.Bool("quiet") {
-		// 	return cli.NewExitError("Configuration doesn't have result section.", 3)
-		// } else {
-		//
-		// }
-		return cli.NewExitError("Configuration doesn't have result section.", 3)
-
-	}
+	// if _, ok := conf[result]; !ok {
+	//
+	// 	// if c.Bool("quiet") {
+	// 	// 	return cli.NewExitError("Configuration doesn't have result section.", 3)
+	// 	// } else {
+	// 	//
+	// 	// }
+	// 	return cli.NewExitError("Configuration doesn't have result section.", 3)
+	//
+	// }
 
 	return nil
+
+}
+
+// Return a unique file name to be used for uploading file.
+func getUniqueFilename() string {
+
+	if dir, err := os.Getwd(); err != nil {
+		return "hoge"
+
+	} else {
+
+		return dir
+
+	}
 
 }
