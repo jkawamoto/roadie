99738c05428611bd8a824c1dec0cd4ca5220f3bf
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-19 17:01:20 -0400

    Implement archive package to make gziped tarballs.

---
+++ archive/archive.go
@@ -0,0 +1,109 @@
+package archive
+
+import (
+	"archive/tar"
+	"bufio"
+	"compress/gzip"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Archive makes a tar.gz file consists of file tree
+func Archive(root string, filename string, excludes []string) error {
+
+	writeFile, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	defer writeFile.Close()
+
+	writer := bufio.NewWriter(writeFile)
+	defer writer.Flush()
+
+	zipWriter, err := gzip.NewWriterLevel(writer, gzip.BestCompression)
+	if err != nil {
+		log.Fatalln(err)
+		return err
+	}
+	defer zipWriter.Close()
+
+	tarWriter := tar.NewWriter(zipWriter)
+	defer tarWriter.Close()
+
+	return filepath.Walk(root, tarballing(tarWriter, excludes))
+
+}
+
+// Create a call back to add founded files to a given archive.
+func tarballing(writer *tar.Writer, excludes []string) filepath.WalkFunc {
+
+	return func(path string, info os.FileInfo, err error) error {
+
+		// Directory won't include the archive.
+		if info.IsDir() {
+			if strings.HasSuffix(path, ".git") {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+
+		// Check the found path matches exclude rules.
+		if m, e := excludePath(path, excludes); e != nil {
+			return e
+		} else if m {
+			return nil
+		}
+
+		// Write a file header.
+		header, err := tar.FileInfoHeader(info, path)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+		header.Name = path
+		writer.WriteHeader(header)
+
+		// Prepare to write a file body.
+		fp, err := os.Open(path)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+		defer fp.Close()
+
+		// Write the body.
+		reader := bufio.NewReader(fp)
+		if _, err := reader.WriteTo(writer); err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+
+		return nil
+
+	}
+
+}
+
+// Check whether a given path is an exclude path.
+func excludePath(path string, excludes []string) (bool, error) {
+
+	for _, part := range strings.Split(filepath.ToSlash(path), "/") {
+
+		for _, pattern := range excludes {
+
+			if match, err := filepath.Match(pattern, part); err != nil {
+				return false, err
+			} else if match {
+				return true, nil
+			}
+		}
+
+	}
+
+	return false, nil
+
+}
