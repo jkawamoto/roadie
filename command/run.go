c8657dabbd10ea53a80a544ab29f68753e429cdb
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-20 00:45:30 -0400

    Update config to script sctucture and move related functions to methods of it.

--- command/run.go
+++ command/run.go
@@ -17,7 +17,10 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-type config map[interface{}]interface{}
+type script struct {
+	filename string
+	body     map[interface{}]interface{}
+}
 
 const (
 	source = "source"
@@ -33,158 +36,61 @@ func CmdRun(c *cli.Context) error {
 
 	yamlFile := c.Args()[0]
 
-	conf := make(config)
-	err := loadScript(yamlFile, c.StringSlice("e"), &conf)
+	s, err := loadScript(yamlFile, c.StringSlice("e"))
 	if err != nil {
 		return cli.NewExitError(err.Error(), 2)
 	}
 
 	// Prepare source section.
 	if v := c.String("git"); v != "" {
-
-		if _, ok := conf[source]; ok {
-			log.Printf(
-				chalk.Red.Color("%s has source section but a Git repository is given. The source section will be overwritten to '%s'."),
-				yamlFile,
-				v,
-			)
-		}
-		conf[source] = v
-
+		s.setGitSource(v)
 	} else if v := c.String("url"); v != "" {
-
-		if _, ok := conf[source]; ok {
-			log.Printf(
-				chalk.Red.Color("%s has source section but a repository URL is given. The source section will be overwritten to '%s'."),
-				yamlFile,
-				v,
-			)
-
-		}
-		conf[source] = v
-
+		s.setURLSource(v)
 	} else if path := c.String("local"); path != "" {
 
-		if _, ok := conf[source]; ok {
-			log.Printf(
-				chalk.Red.Color("%s has source section but a path for source codes is given. The source section will be overwritten."),
-				yamlFile,
-			)
-		}
-
-		info, notExists := os.Stat(path)
-		if notExists != nil {
-			// Target path does not exits.
-			return cli.NewExitError(notExists.Error(), 2)
-
-		}
-
-		// TODO: Check bucker is specified.
+		project := c.String("project")
 		bucket := c.String("bucket")
-		if bucket == "" {
-			return cli.NewExitError("bucket flag is required to use local files as the source code.", 2)
+		if project == "" || bucket == "" {
+			return cli.NewExitError("project and bucket flags are required when you use --local", 2)
 		}
-
-		var arcPath string
-		var location *url.URL
-		if info.IsDir() {
-
-			filename := basename(yamlFile) + time.Now().Format("20060102150405") + ".tar.gz"
-			arcPath = filepath.Join(os.TempDir(), filename)
-			log.Printf("Create an archived file %s", arcPath)
-			util.Archive(path, arcPath, nil)
-
-			location = createURL(bucket, filename)
-
-		} else {
-
-			arcPath = path
-			location = createURL(bucket, basename(path))
-
-		}
-
-		// TODO: Upload the archive file to url.
-		log.Printf("Uploading to %s", location)
-		storage, gcsErr := util.NewStorage(c.String("project"), c.String("bucket"))
-		if gcsErr != nil {
-			return cli.NewExitError(gcsErr.Error(), 2)
-		}
-
-		storage.Upload(arcPath, location)
-
-		conf[source] = location.String()
-
-	} else {
-		// TODO: if no source flag given, what shoud it do?
+		s.setLocalSource(path, project, bucket)
 
 	}
 
 	// Check result section.
-	// checkResultSection
+	if _, ok := s.body["result"]; !ok {
+
+		project := c.String("project")
+		bucket := c.String("bucket")
+		if project == "" || bucket == "" {
+			// return cli.NewExitError("project and bucket flags are required when you use --local", 2)
+		}
+
+	}
 
 	// debug:
-	res, err := yaml.Marshal(conf)
-	if err != nil {
-		return cli.NewExitError(err.Error(), 1)
-	}
-	fmt.Println(string(res))
+	fmt.Println(s.String())
 
 	return nil
-}
-
-// Load a given script file and apply arguments.
-func loadScript(filename string, args []string, out *config) error {
-
-	// Define function map to replace place holders.
-	funcs := template.FuncMap{}
-	for _, v := range args {
-		sp := strings.Split(v, "=")
-		if len(sp) >= 2 {
-			funcs[sp[0]] = func() string {
-				return sp[1]
-			}
-		}
-	}
-
-	// Load YAML config file.
-	conf, err := template.New(filename).Funcs(funcs).ParseFiles(filename)
-	if err != nil {
-		switch err.(type) {
-		case *os.PathError:
-			return err
-		default:
-			return fmt.Errorf("Cannot apply variables to the place holders in %s", filename)
-		}
-	}
-
-	// Replace place holders with given args.
-	buf := &bytes.Buffer{}
-	if err := conf.Execute(buf, nil); err != nil {
-		return err
-	}
-
-	// Unmarshal YAML file.
-	return yaml.Unmarshal(buf.Bytes(), out)
-
 }
 
 // checkResultSection validates config has result section.
-func checkResultSection(c *cli.Context, conf *config) error {
-
-	// if _, ok := conf[result]; !ok {
-	//
-	// 	// if c.Bool("quiet") {
-	// 	// 	return cli.NewExitError("Configuration doesn't have result section.", 3)
-	// 	// } else {
-	// 	//
-	// 	// }
-	// 	return cli.NewExitError("Configuration doesn't have result section.", 3)
-	//
-	// }
-
-	return nil
-
-}
+// func checkResultSection(c *cli.Context, conf *config) error {
+//
+// 	// if _, ok := conf[result]; !ok {
+// 	//
+// 	// 	// if c.Bool("quiet") {
+// 	// 	// 	return cli.NewExitError("Configuration doesn't have result section.", 3)
+// 	// 	// } else {
+// 	// 	//
+// 	// 	// }
+// 	// 	return cli.NewExitError("Configuration doesn't have result section.", 3)
+// 	//
+// 	// }
+//
+// 	return nil
+//
+// }
 
 // Get the basename of a given filename.
 func basename(filename string) string {
@@ -205,4 +111,120 @@ func createURL(bucket, name string) *url.URL {
 		Path:   filepath.Join("/.roadie/source/", name),
 	}
 
+}
+
+// Load a given script file and apply arguments.
+func loadScript(filename string, args []string) (*script, error) {
+
+	// Define function map to replace place holders.
+	funcs := template.FuncMap{}
+	for _, v := range args {
+		sp := strings.Split(v, "=")
+		if len(sp) >= 2 {
+			funcs[sp[0]] = func() string {
+				return sp[1]
+			}
+		}
+	}
+
+	// Load YAML config file.
+	conf, err := template.New(filename).Funcs(funcs).ParseFiles(filename)
+	if err != nil {
+		switch err.(type) {
+		case *os.PathError:
+			return nil, err
+		default:
+			return nil, fmt.Errorf("Cannot apply variables to the place holders in %s", filename)
+		}
+	}
+
+	// Replace place holders with given args.
+	buf := &bytes.Buffer{}
+	if err := conf.Execute(buf, nil); err != nil {
+		return nil, err
+	}
+
+	// Unmarshal YAML file.
+	body := make(map[interface{}]interface{})
+	if err := yaml.Unmarshal(buf.Bytes(), body); err != nil {
+		return nil, err
+	}
+
+	return &script{
+		filename: filename,
+		body:     body,
+	}, nil
+
+}
+
+// Set a git repository to source section.
+func (s *script) setGitSource(repo string) {
+	if _, ok := s.body[source]; ok {
+		log.Printf(
+			chalk.Red.Color("%s has source section but a Git repository is given. The source section will be overwritten to '%s'."),
+			s.filename, repo)
+	}
+	s.body[source] = repo
+}
+
+// Set a URL to source section.
+func (s *script) setURLSource(url string) {
+	if _, ok := s.body[source]; ok {
+		log.Printf(
+			chalk.Red.Color("%s has source section but a repository URL is given. The source section will be overwritten to '%s'."),
+			s.filename, url)
+	}
+	s.body[source] = url
+}
+
+// Upload source files and set that location to source section.
+func (s *script) setLocalSource(path, project, bucket string) error {
+	if _, ok := s.body[source]; ok {
+		log.Printf(
+			chalk.Red.Color("%s has source section but a path for source codes is given. The source section will be overwritten."),
+			s.filename)
+	}
+
+	info, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	var arcPath string
+	var location *url.URL
+	if info.IsDir() {
+
+		filename := basename(s.filename) + time.Now().Format("20060102150405") + ".tar.gz"
+		arcPath = filepath.Join(os.TempDir(), filename)
+		log.Printf("Create an archived file %s", arcPath)
+		if err := util.Archive(path, arcPath, nil); err != nil {
+			return err
+		}
+		location = createURL(bucket, filename)
+
+	} else {
+
+		arcPath = path
+		location = createURL(bucket, basename(path))
+
+	}
+
+	log.Printf("Uploading to %s", location)
+	storage, err := util.NewStorage(project, bucket)
+	if err != nil {
+		return err
+	}
+	if err := storage.Upload(arcPath, location); err != nil {
+		return err
+	}
+
+	s.body[source] = location.String()
+	return nil
+
+}
+
+// Convert to string.
+func (s *script) String() string {
+	res, _ := yaml.Marshal(s.body)
+	return string(res)
 }
