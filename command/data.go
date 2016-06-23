7e2b9c82b8643b600ae456912de97a61cf3011b5
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-23 12:40:36 -0400

    Implement data command.

---
+++ command/data.go
@@ -0,0 +1,32 @@
+package command
+
+import (
+	"fmt"
+
+	"github.com/ttacon/chalk"
+	"github.com/urfave/cli"
+)
+
+// DataPrefix defines a prefix to store data files.
+const DataPrefix = ".roadie/data"
+
+// CmdDataPut uploads a given file.
+func CmdDataPut(c *cli.Context) error {
+
+	n := c.NArg()
+	if n < 1 || n > 2 {
+		fmt.Printf(chalk.Red.Color("expected 1 or 2 arguments. (%d given)\n"), c.NArg())
+		return cli.ShowSubcommandHelp(c)
+	}
+
+	conf := GetConfig(c)
+	var name string
+	if n == 1 {
+		name = ""
+	} else {
+		name = c.Args()[1]
+	}
+	_, err := UploadToGCS(conf.Gcp.Project, conf.Gcp.Bucket, DataPrefix, name, c.Args()[0])
+	return err
+
+}
