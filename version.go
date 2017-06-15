b3ce1b1fd55887ef0a422d5c1dd8a0aa2992738a
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-06-15 01:06:38 -0400

    Bump version: 0.3.8 → 0.3.9

--- version.go
+++ version.go
@@ -25,7 +25,7 @@ package main
 const Name string = "roadie"
 
 // Version defines current version number.
-const Version string = "0.3.8"
+const Version string = "0.3.9"
 
 // Author defines the author of this program.
 const Author string = "Junpei Kawamoto"
