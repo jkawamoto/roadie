b2d27b7579c7a9c6b52e4358b383a472cee5e930
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-09-22 13:17:39 -0400

    Fixed the bug that Archive adds root directory name for each file's name.

--- command/util/archive_test.go
+++ command/util/archive_test.go
@@ -22,59 +22,191 @@
 package util
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"io/ioutil"
 	"os"
-	"os/exec"
 	"path"
 	"path/filepath"
-	"strings"
 	"testing"
 )
 
-const archiveFile = "test.tar.gz"
-
 func TestArchive(t *testing.T) {
 
-	temp := os.TempDir()
-	target := path.Join(temp, archiveFile)
-	t.Logf("Creating an archive file: %s", target)
+	var err error
+	root := ".."
 
-	if err := Archive("..", target, []string{"*.tar"}); err != nil {
-		t.Error(err.Error())
+	cases := []struct {
+		excludes []string
+	}{
+		{nil},
+		{[]string{"*_test.go"}},
 	}
 
-	root, err := os.Getwd()
-	if err != nil {
-		t.Error(err.Error())
-	}
+	for _, c := range cases {
 
-	os.Chdir(temp)
-	defer func() {
-		os.Chdir(root)
-	}()
-	exec.Command("tar", "-zxvf", archiveFile)
+		expectedFiles := make(map[string]struct{})
+		err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 
-	if err := filepath.Walk(path.Join(root, ".."), checkExistence(temp)); err != nil {
-		t.Error(err.Error())
+			if err != nil {
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+
+			var matched bool
+			for _, e := range c.excludes {
+				matched, err = filepath.Match(e, info.Name())
+				if err != nil {
+					return err
+				} else if matched {
+					return nil
+				}
+			}
+
+			path, err = filepath.Rel(root, path)
+			if err != nil {
+				return err
+			}
+			// Archived files' names shoule be slashed.
+			expectedFiles[filepath.ToSlash(path)] = struct{}{}
+			return nil
+
+		})
+		if err != nil {
+			t.Fatalf("Walking a directory returns an error: %v", err)
+		}
+
+		temp, err := ioutil.TempDir("", "TestArchive")
+		if err != nil {
+			t.Fatalf("TempDir returns an error: %v", err)
+		}
+		defer os.RemoveAll(temp)
+
+		target := path.Join(temp, "test.tar.gz")
+		t.Logf("Creating an archive file: %s", target)
+		err = Archive(root, target, c.excludes)
+		if err != nil {
+			t.Fatalf("Archive returns an error: %v", err)
+		}
+
+		f, err := os.Open(target)
+		if err != nil {
+			t.Fatalf("Open(%v) returns an error: %v", target, err)
+		}
+		defer f.Close()
+
+		zipReader, err := gzip.NewReader(f)
+		if err != nil {
+			t.Fatalf("gzip.NewReader returns an error: %v", err)
+		}
+		defer zipReader.Close()
+
+		tarReader := tar.NewReader(zipReader)
+		var header *tar.Header
+		var expected, received []byte
+		for {
+
+			header, err = tarReader.Next()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				t.Fatalf("reading tarball header returns an error: %v", err)
+			}
+
+			expected, err = ioutil.ReadFile(filepath.Join(root, header.Name))
+			if err != nil {
+				t.Fatalf("cannot read %v: %v", header.Name, err)
+			}
+			received, err = ioutil.ReadAll(tarReader)
+			if err != nil {
+				t.Fatalf("reading a file from a tarball returns an error: %v", err)
+			}
+			if string(received) != string(expected) {
+				t.Errorf("received %v, want %v", string(received), string(expected))
+			}
+			delete(expectedFiles, header.Name)
+
+		}
+
+		if len(expectedFiles) != 0 {
+			t.Errorf("archived file misses %v", expectedFiles)
+		}
+
 	}
 
 }
 
-func checkExistence(target string) filepath.WalkFunc {
+// TestArchiveFile tests archiving only one file by Archive.
+func TestArchiveFile(t *testing.T) {
 
-	return func(path string, info os.FileInfo, err error) error {
+	target := "archive.go"
 
-		if info.IsDir() {
-			if strings.HasSuffix(path, ".git") {
-				return filepath.SkipDir
-			}
-			return nil
-		} else if strings.HasSuffix(path, ".tar") {
-			return nil
+	temp, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatalf("TempDir returns an error: %v", err)
+	}
+	defer os.RemoveAll(temp)
+
+	filename := filepath.Join(temp, target+".tar.gz")
+	err = Archive(target, filename, nil)
+	if err != nil {
+		t.Fatalf("Archive returns an error: %v", err)
+	}
+
+	f, err := os.Open(filename)
+	if err != nil {
+		t.Fatalf("Open(%v) returns an error: %v", filename, err)
+	}
+	defer f.Close()
+
+	zipReader, err := gzip.NewReader(f)
+	if err != nil {
+		t.Fatalf("gzip.NewReader returns an error: %v", err)
+	}
+	defer zipReader.Close()
+
+	tarReader := tar.NewReader(zipReader)
+	var header *tar.Header
+	var expected, received []byte
+	for {
+
+		header, err = tarReader.Next()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			t.Fatalf("reading tarball header returns an error: %v", err)
 		}
 
-		_, check := os.Stat(path)
-		return check
+		expected, err = ioutil.ReadFile(header.Name)
+		if err != nil {
+			t.Fatalf("cannot read %v: %v", header.Name, err)
+		}
+		received, err = ioutil.ReadAll(tarReader)
+		if err != nil {
+			t.Fatalf("reading a file from a tarball returns an error: %v", err)
+		}
+		if string(received) != string(expected) {
+			t.Errorf("received %v, want %v", string(received), string(expected))
+		}
 
+	}
+
+}
+
+func TestArchiveNotExistingDirectory(t *testing.T) {
+
+	temp, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatalf("TempDir returns an error: %v", err)
+	}
+	defer os.RemoveAll(temp)
+
+	err = Archive("../_util", filepath.Join(temp, "test.tar.gz"), nil)
+	if err == nil {
+		t.Error("Archiving a not existing directory returns no errors")
 	}
 
 }
