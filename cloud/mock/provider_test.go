a276d220e4b254e79e007430b2086fe3b6b68be1
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-09-23 11:52:50 -0400

    Implement a mock resource manager.

--- cloud/mock/provider_test.go
+++ cloud/mock/provider_test.go
@@ -26,16 +26,71 @@ import (
 	"testing"
 )
 
+func TestInstanceManager(t *testing.T) {
+
+	ctx := context.Background()
+	p := NewProvider()
+	m, err := p.InstanceManager(ctx)
+	if err != nil {
+		t.Fatalf("InstanceManager returns an error: %v", err)
+	}
+
+	if _, ok := m.(*InstanceManager); !ok {
+		t.Errorf("InstanceManager doesn't return a mock manager: %T", m)
+	}
+
+	// With a canceled context.
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+	_, err = p.InstanceManager(ctx)
+	if err == nil {
+		t.Error("context is canceled but no errors are returned")
+	}
+
+}
+
 func TestStorageManager(t *testing.T) {
 
+	ctx := context.Background()
 	p := NewProvider()
-	m, err := p.StorageManager(context.Background())
+	m, err := p.StorageManager(ctx)
 	if err != nil {
 		t.Fatalf("StorageManager returns an error: %v", err)
 	}
 
 	if _, ok := m.(*StorageManager); !ok {
 		t.Errorf("StorageManager doesn't return a mock manager: %T", m)
+	}
+
+	// With a canceled context.
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+	_, err = p.StorageManager(ctx)
+	if err == nil {
+		t.Error("context is canceled but no errors are returned")
+	}
+
+}
+
+func TestResouceManager(t *testing.T) {
+
+	ctx := context.Background()
+	p := NewProvider()
+	m, err := p.ResourceManager(ctx)
+	if err != nil {
+		t.Fatalf("ResourceManager returns an error: %v", err)
+	}
+
+	if _, ok := m.(*ResourceManager); !ok {
+		t.Errorf("ResourceManager doesn't return a mock manager: %T", m)
+	}
+
+	// With a canceled context.
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+	_, err = p.ResourceManager(ctx)
+	if err == nil {
+		t.Error("context is canceled but no errors are returned")
 	}
 
 }
