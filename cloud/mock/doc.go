a4d09526c67f93164d61f68d580b3f2422491725
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-09-29 12:49:17 -0400

    Add doc.go to mock package.

---
+++ cloud/mock/doc.go
@@ -0,0 +1,24 @@
+//
+// cloud/mock/doc.go
+//
+// Copyright (c) 2016-2017 Junpei Kawamoto
+//
+// This file is part of Roadie.
+//
+// Roadie is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Roadie is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Roadie.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// Package mock provides a mock service provider which implements cloud.Provider
+// interface.
+package mock
